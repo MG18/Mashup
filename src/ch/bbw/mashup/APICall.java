6bba7d71d939accf0a8288194d86c2b908fa00db
Mehmet Guel <mehmet_gul101@hotmail.com>
2019-02-02 19:27:48 +0100

    Added Class StationBoard

--- src/ch/bbw/mashup/APICall.java
+++ src/ch/bbw/mashup/APICall.java
@@ -18,5 +18,32 @@ public class APICall {
 	private StringBuilder 		response;
 	private URL					url;
 	private HttpURLConnection 	connection;
+	
+	public String getResponse(String url) {
+		try {
+			this.url = new URL(url);
+			connection = (HttpURLConnection) this.url.openConnection();
+			connection.setRequestMethod("GET");
+
+			input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			this.response = new StringBuilder();
+			inputLine = "";
+
+			while ((inputLine = input.readLine()) != null) {
+
+				response.append(inputLine);
+
+			}
+
+			input.close();
+			
+			return response.toString();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
 
 }
--- src/ch/bbw/mashup/Koordinaten.java
+++ src/ch/bbw/mashup/Koordinaten.java
@@ -1,5 +1,36 @@
 package ch.bbw.mashup;
 
+/**
+ * @author  Mehmet Gül
+ * @version 1.0
+ *
+ */
+
 public class Koordinaten {
+	
+	private double xKoordinate;
+	private double yKoordinate;
+	
+	
+	public Koordinaten(Double xKoordinate, Double yKoordinate) {
+		
+		this.xKoordinate = xKoordinate;
+		this.yKoordinate = yKoordinate;
+		
+	}
+	public double getxKoordinate() {
+		return xKoordinate;
+	}
+	public void setxKoordinate(double xKoordinate) {
+		this.xKoordinate = xKoordinate;
+	}
+	public double getyKoordinate() {
+		return yKoordinate;
+	}
+	public void setyKoordinate(double yKoordinate) {
+		this.yKoordinate = yKoordinate;
+	}
+	
+	
 
 }
