4ada391dbe1d66646fb3642416b97ca8d7ae1a8d
Mehmet Guel <mehmet_gul101@hotmail.com>
2019-02-02 19:14:36 +0100

    Added Attributes to the APICall Class

--- src/ch/bbw/mashup/APICall.java
+++ src/ch/bbw/mashup/APICall.java
@@ -1,5 +1,22 @@
 package ch.bbw.mashup;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @author  Mehmet Gül
+ * @version 1.0
+ *
+ */
+
 public class APICall {
+	
+	private BufferedReader      input;
+	private String				inputLine;
+	private StringBuilder 		response;
+	private URL					url;
+	private HttpURLConnection 	connection;
 
 }
